ce78becb1047294fc9aa62f48708d46a49e1873d
Bradley Gillap <bradgillap@gmail.com>
2018-01-12 11:40:03 -0500

    Update substratdb.sql
    fixed timestamp
    
    Tables actually insert now.

--- substratdb.sql
+++ substratdb.sql
@@ -4,9 +4,9 @@ CREATE TABLE item(
 	itemDesc varchar(255) DEFAULT 'No description yet.',
 	itemType varchar(30) DEFAULT 'Unspecified',
 	itemGameId varchar(30) DEFAULT 'Unspecified',
-	itemIsCraftable bin(1) NOT NULL,
-	itemIsRaw bin(1) NOT NULL,
-	itemUpdated timestamp DEFAULT CURRENT_TIMESTAMP(),
+	itemIsCraftable BINARY NOT NULL,
+	itemIsRaw BINARY NOT NULL,
+	itemUpdated timestamp DEFAULT CURRENT_TIMESTAMP,
 	UNIQUE (itemId),
 	PRIMARY KEY (itemId)
 );
@@ -14,7 +14,7 @@ CREATE TABLE item(
 CREATE TABLE recipe(
 	recipeId smallint NOT NULL AUTO_INCREMENT,
 	itemId smallint NOT NULL,
-	recipeUpdated timestamp DEFAULT CURRENT_TIMESTAMP(),
+	recipeUpdated timestamp DEFAULT CURRENT_TIMESTAMP,
 	UNIQUE (recipeId),
 	PRIMARY KEY (recipeId),
 	FOREIGN KEY (itemId) REFERENCES item(itemId)
