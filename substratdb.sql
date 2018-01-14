f769777bc8a43f84472220b6bed695199dd99e44
Bradley Gillap <bradgillap@gmail.com>
2018-01-14 16:09:22 -0500

    Update substratdb.sql

--- substratdb.sql
+++ substratdb.sql
@@ -21,7 +21,7 @@ CREATE TABLE recipe(
 	FOREIGN KEY (itemId) REFERENCES item(itemId)
 );
 
-INSERT INTO item ( itemName, itemDesc, itemType, itemGameId, itemIsCraftable, itemIsRaw )
+INSERT INTO item ( itemName, itemDesc, itemType, itemGameId, itemMenu, itemIsCraftable, itemIsRaw )
 VALUES	('Titanium',
 	'Titanium is a raw material that is heavily used in crafting. it is used to craft almost all Alterra technology, including Equipment, Tools, Vehicles and Seabase modules. It can be found in Limestone Outcrops, crafted from metal salvage, or obtained by using the Scanner to scan fragments for Blueprints that the player already has. Titanium can also be found as a large resource deposit.',
 	'Raw Resource',
