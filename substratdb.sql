1ffc2f33189f51f3c62b9ab74064c934544a06fa
Bradley Gillap <bradgillap@gmail.com>
2018-01-16 10:35:59 -0500

    Update substratdb.sql

--- substratdb.sql
+++ substratdb.sql
@@ -1,22 +1,22 @@
-CREATE TABLE item(
-	itemId smallint NOT NULL AUTO_INCREMENT,
-	itemName varchar(50) NOT NULL,
-	itemDesc TEXT NOT NULL,
-	itemGameId varchar(50) DEFAULT 'Unspecified',
-	itemMenu varchar(30) DEFAULT 'Unspecified',
-	itemMadeWith varchar(30) DEFAULT 'Unspecified',
-	itemPCapacity smallint,
-	itemPPM smallint,
-	itemPConsumption smallint,
-	itemHullIntegrity tinyint,
-	itemEnergy smallint,
-	itemBuildTime tinyint,
-	itemSize tinyint,
-	itemIsCraftable BOOLEAN NOT NULL,
-	itemIsRaw BOOLEAN NOT NULL,
-	itemUpdated timestamp DEFAULT CURRENT_TIMESTAMP,
-	UNIQUE (itemId),
-	PRIMARY KEY (itemId)
+CREATE TABLE IF NOT EXISTS `item` (
+	`itemId` smallint(6) NOT NULL AUTO_INCREMENT,
+	`itemName` varchar(50) NOT NULL,
+	`itemDesc` text NOT NULL,
+	`itemGameId` varchar(50) DEFAULT 'Unspecified' 				COMMENT 'Actual ID in the game code.',
+	`itemMenu` varchar(30) DEFAULT 'Unspecified'				COMMENT 'Menu category the item remains in.',
+	`itemMadeWith` varchar(30) DEFAULT 'Unspecified' 			COMMENT 'What type of device is used to craft the item.',
+	`itemPCapacity` smallint(6) DEFAULT NULL 					COMMENT 'Electricity item can store.',
+	`itemPPM` smallint(6) DEFAULT NULL 							COMMENT 'Power generated per minute.',
+	`itemPConsumption` smallint(6) DEFAULT NULL 				COMMENT 'Power consumed per minute.',  
+	`itemHullIntegrity` tinyint(4) DEFAULT NULL 				COMMENT 'Hull integrity added to bases with this item.',
+	`itemEnergy` smallint(6) DEFAULT NULL 						COMMENT 'Power item will convert to in the bioreactor.',
+	`itemBuildTime` tinyint(4) DEFAULT NULL 					COMMENT 'Time it takes to craft item.',
+	`itemSize` tinyint(4) DEFAULT NULL 							COMMENT 'Inventory space item uses.',
+	`itemIsCraftable` tinyint(1) NOT NULL 						COMMENT 'Item can be crafted.',
+	`itemIsRaw` tinyint(1) NOT NULL 							COMMENT 'Item is raw.',
+	`itemUpdated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP 	COMMENT 'Last time dastabase line was changed.',
+	PRIMARY KEY (`itemId`),
+	UNIQUE KEY `itemId` (`itemId`)
 );
 
 CREATE TABLE recipe(
