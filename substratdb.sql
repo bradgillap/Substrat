8533e0ddfd93e2feb00268dfcc78e4f5472418a8
Bradley Gillap <bradgillap@users.noreply.github.com>
2018-01-15 21:06:32 -0500

    More columns

--- substratdb.sql
+++ substratdb.sql
@@ -5,7 +5,8 @@ CREATE TABLE item(
 	itemGameId varchar(50) DEFAULT 'Unspecified',
 	itemMenu varchar(30) DEFAULT 'Unspecified',
 	itemMadeWith varchar(30) DEFAULT 'Unspecified',
-	itemEnergy smallint
+	itemHullIntegrity tinyint,
+	itemEnergy smallint,
 	itemBuildTime tinyint,
 	itemSize tinyint,
 	itemIsCraftable BOOLEAN NOT NULL,
@@ -26,205 +27,235 @@ CREATE TABLE recipe(
 
 /*Basic Materials */
 
-INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
+INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
 VALUES	(
 		'Bleach',
 		'NaCIO. Sodium hypochlorite bleach. Sanitizing applications.',
 		'bleach',
 		'Basic Materials',
 		'Fabricator',
-		0,
-		1,
-		1,
-		1,
-		0
+		0,	/*hull integrity*/
+		0,	
+		1,	/*Build Time*/
+		1,	/*size*/
+		1,	/*craftable*/
+		0	/*raw*/
 	);
 
-INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
+INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
 VALUES	(
 		'Enameled Glass',
 		'Glass, hardened using a natural substrate.',
 		'enameledglass',
 		'Basic Materials',
 		'Fabricator',
+		0,	/*hull integrity*/
 		0,
-		1,
-		1,
-		1,
-		0
+		1,	/*Build Time*/
+		1,	/*size*/
+		1,	/*craftable*/
+		0	/*raw*/
 	);
 
 
-INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
+INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
 VALUES	(
 		'Fiber Mesh',
 		'Strong natural fiber. Comfy',
 		'fibermesh',
 		'Basic Materials',
 		'Fabricator',
+		0,	/*hull integrity*/
 		0,
-		1,
-		1,
-		1,
-		0
+		1,	/*Build Time*/
+		1,	/*size*/
+		1,	/*craftable*/
+		0	/*raw*/
 	);
 
 
-INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
+INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
 VALUES	(
 		'Glass',
 		'SiO4. Pure fused quartz glass.',
 		'glass',
 		'Basic Materials',
 		'Fabricator',
+		0,	/*hull integrity*/
 		0,
-		1,
-		1,
-		1,
-		0
+		1,	/*Build Time*/
+		1,	/*size*/
+		1,	/*craftable*/
+		0	/*raw*/
 	);
 
 
-INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
+INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
 VALUES	(
 		'Lubricant',
 		'Naturally-derived, oil-based lubrican. Industrial applications.',
 		'lubricant',
 		'Basic Materials',
 		'Fabricator',
+		0,	/*hull integrity*/
 		100,
-		1,
-		1,
-		1,
-		0
+		1,	/*Build Time*/
+		1,	/*size*/
+		1,	/*craftable*/
+		0	/*raw*/
 	);
 
 
-INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
+INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
 VALUES	(
 		'Plasteel Ingot',
 		'Ultra-strong synthetic construction material.',
 		'plasteelingot',
 		'Basic Materials',
 		'Fabricator',
+		0,	/*hull integrity*/
 		0,
-		1,
-		1,
-		1,
-		0
+		1,	/*Build Time*/
+		1,	/*size*/
+		1,	/*craftable*/
+		0	/*raw*/
 	);
 
-INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
+INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
 VALUES	(
 		'Silicone Rubber',
 		'Synthetic, silicone-based rubber.',
 		'silicone',
 		'Basic Materials',
 		'Fabricator',
+		0,	/*hull integrity*/
 		0,
-		1,
-		1,
-		1,
-		0
+		1,	/*Build Time*/
+		1,	/*size*/
+		1,	/*craftable*/
+		0	/*raw*/
 	);
 
-INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
+INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
 VALUES	(
 		'Titanium Ingot',
 		'Ti. Condensed titanium bar.',
 		'titaniumingot',
 		'Basic Materials',
 		'Fabricator',
+		0,	/*hull integrity*/
 		0,
-		5,
-		1,
-		1,
-		0
+		5,	/*Build Time*/
+		1,	/*size*/
+		1,	/*craftable*/
+		0	/*raw*/
 	);
 
-INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
+INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
 VALUES	(
 		'Titanium',
 		'Ti. Basic building material.',
 		'titanium',
 		'Basic Materials',
 		'Fabricator',
+		0,	/*hull integrity*/
 		0,
-		1,
-		1,
-		1,
-		1
+		1,	/*Build Time*/
+		1,	/*size*/
+		1,	/*craftable*/
+		1	/*raw*/
 	);
 
 /* Advanced Materials */
 
-INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
+INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
 VALUES	(
 		'Aerogel',
 		'Light, Porous, dried gel. High heat insulation.',
 		'aerogel',
 		'Advanced Materials',
 		'Fabricator',
+		0,	/*hull integrity*/
 		0,
-		1,
-		1,
-		1,
-		0
+		1,	/*Build Time*/
+		1,	/*size*/
+		1,	/*craftable*/
+		0	/*raw*/
 	);
 
-INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
+INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
 VALUES	(
 		'Benzene',
 		'C6H6. Oil and solvent applications.',
 		'benzene',
 		'Advanced Materials',
 		'Fabricator',
+		0,	/*hull integrity*/
 		0,
-		1,
-		1,
-		1,
-		0
+		1,	/*Build Time*/
+		1,	/*size*/
+		1,	/*craftable*/
+		0	/*raw*/
 	);
 
-INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
+INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
 VALUES	(
 		'Hydrochloric Acid',
 		'HCI. Powerful acidic compound. Employ due care.',
 		'hydrochloricacid',
 		'Advanced Materials',
 		'Fabricator',
+		0, /*hull integrity*/
 		0,
-		1,
-		1,
-		1,
-		0
+		1,	/*Build Time*/
+		1,	/*size*/
+		1,	/*craftable*/
+		0	/*raw*/
 	);
 	
-INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
+INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
 VALUES	(
 		'Polyaniline',
 		'High conductance polymer.',
 		'polyaniline',
 		'Advanced Materials',
 		'Fabricator',
+		0, /*hull integrity*/
 		0,
-		4,
-		1,
-		1,
-		0
+		4,	/*Build Time*/
+		1,	/*size*/
+		1,	/*craftable*/
+		0	/*raw*/
 	);
 	
-INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
+INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
 VALUES	(
 		'Synthetic Fibers',
 		'High-strength synthetic fibers.',
 		'aramidfibers',
 		'Advanced Materials',
 		'Fabricator',
+		0, /*hull integrity*/
 		0,
-		4,
-		1,
-		1,
-		0
+		4,	/*Build Time*/
+		1,	/*size*/
+		1,	/*craftable*/
+		0	/*raw*/
 	);
-	
+
+/* Base Pieces */
+
+INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
+VALUES	(
+		'Synthetic Fibers',
+		'High-strength synthetic fibers.',
+		'aramidfibers',
+		'Advanced Materials',
+		'Fabricator',
+		0, /*hull integrity*/
+		0,
+		4,	/*Build Time*/
+		1,	/*size*/
+		1,	/*craftable*/
+		0	/*raw*/
+	);
