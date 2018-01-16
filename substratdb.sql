a69873ce5fed67793c93efb5b2f37cea6d561c79
Bradley Gillap <bradgillap@users.noreply.github.com>
2018-01-15 21:24:54 -0500

    power consumption

--- substratdb.sql
+++ substratdb.sql
@@ -5,6 +5,9 @@ CREATE TABLE item(
 	itemGameId varchar(50) DEFAULT 'Unspecified',
 	itemMenu varchar(30) DEFAULT 'Unspecified',
 	itemMadeWith varchar(30) DEFAULT 'Unspecified',
+	itemPCapacity smallint,
+	itemPPM smallint,
+	itemPConsumption smallint,
 	itemHullIntegrity tinyint,
 	itemEnergy smallint,
 	itemBuildTime tinyint,
@@ -27,30 +30,36 @@ CREATE TABLE recipe(
 
 /*Basic Materials */
 
-INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
+INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemPCapacity, itemPPM, itemPConsumption, itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
 VALUES	(
 		'Bleach',
 		'NaCIO. Sodium hypochlorite bleach. Sanitizing applications.',
 		'bleach',
 		'Basic Materials',
 		'Fabricator',
+		0,	/* Power Capacity */
+		0,	/* Power Per Minute */
+		0,	/* Power Consumption Per Minute */
 		0,	/*hull integrity*/
-		0,	
+		0,	/*BioReactor Energy*/
 		1,	/*Build Time*/
 		1,	/*size*/
 		1,	/*craftable*/
 		0	/*raw*/
 	);
 
-INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
+INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemPCapacity, itemPPM, itemPConsumption, itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
 VALUES	(
 		'Enameled Glass',
 		'Glass, hardened using a natural substrate.',
 		'enameledglass',
 		'Basic Materials',
 		'Fabricator',
+		0,	/* Power Capacity */
+		0,	/* Power Per Minute */
+		0,	/* Power Consumption Per Minute */
 		0,	/*hull integrity*/
-		0,
+		0,	/*BioReactor Energy*/
 		1,	/*Build Time*/
 		1,	/*size*/
 		1,	/*craftable*/
@@ -58,15 +67,18 @@ VALUES	(
 	);
 
 
-INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
+INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemPCapacity, itemPPM, itemPConsumption, itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
 VALUES	(
 		'Fiber Mesh',
 		'Strong natural fiber. Comfy',
 		'fibermesh',
 		'Basic Materials',
 		'Fabricator',
+		0,	/* Power Capacity */
+		0,	/* Power Per Minute */
+		0,	/* Power Consumption Per Minute */
 		0,	/*hull integrity*/
-		0,
+		0,	/*BioReactor Energy*/
 		1,	/*Build Time*/
 		1,	/*size*/
 		1,	/*craftable*/
@@ -74,15 +86,18 @@ VALUES	(
 	);
 
 
-INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
+INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemPCapacity, itemPPM, itemPConsumption, itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
 VALUES	(
 		'Glass',
 		'SiO4. Pure fused quartz glass.',
 		'glass',
 		'Basic Materials',
 		'Fabricator',
+		0,	/* Power Capacity */
+		0,	/* Power Per Minute */
+		0,	/* Power Consumption Per Minute */
 		0,	/*hull integrity*/
-		0,
+		0,	/*BioReactor Energy*/
 		1,	/*Build Time*/
 		1,	/*size*/
 		1,	/*craftable*/
@@ -90,15 +105,18 @@ VALUES	(
 	);
 
 
-INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
+INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemPCapacity, itemPPM, itemPConsumption, itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
 VALUES	(
 		'Lubricant',
 		'Naturally-derived, oil-based lubrican. Industrial applications.',
 		'lubricant',
 		'Basic Materials',
 		'Fabricator',
+		0,	/* Power Capacity */
+		0,	/* Power Per Minute */
+		0,	/* Power Consumption Per Minute */
 		0,	/*hull integrity*/
-		100,
+		100,	/*BioReactor Energy*/
 		1,	/*Build Time*/
 		1,	/*size*/
 		1,	/*craftable*/
@@ -106,60 +124,72 @@ VALUES	(
 	);
 
 
-INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
+INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemPCapacity, itemPPM, itemPConsumption, itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
 VALUES	(
 		'Plasteel Ingot',
 		'Ultra-strong synthetic construction material.',
 		'plasteelingot',
 		'Basic Materials',
 		'Fabricator',
+		0,	/* Power Capacity */
+		0,	/* Power Per Minute */
+		0,	/* Power Consumption Per Minute */
 		0,	/*hull integrity*/
-		0,
+		0,	/*BioReactor Energy*/
 		1,	/*Build Time*/
 		1,	/*size*/
 		1,	/*craftable*/
 		0	/*raw*/
 	);
 
-INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
+INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemPCapacity, itemPPM, itemPConsumption, itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
 VALUES	(
 		'Silicone Rubber',
 		'Synthetic, silicone-based rubber.',
 		'silicone',
 		'Basic Materials',
 		'Fabricator',
+		0,	/* Power Capacity */
+		0,	/* Power Per Minute */
+		0,	/* Power Consumption Per Minute */
 		0,	/*hull integrity*/
-		0,
+		0,	/*BioReactor Energy*/
 		1,	/*Build Time*/
 		1,	/*size*/
 		1,	/*craftable*/
 		0	/*raw*/
 	);
 
-INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
+INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemPCapacity, itemPPM, itemPConsumption, itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
 VALUES	(
 		'Titanium Ingot',
 		'Ti. Condensed titanium bar.',
 		'titaniumingot',
 		'Basic Materials',
 		'Fabricator',
+		0,	/* Power Capacity */
+		0,	/* Power Per Minute */
+		0,	/* Power Consumption Per Minute */
 		0,	/*hull integrity*/
-		0,
+		0,	/*BioReactor Energy*/
 		5,	/*Build Time*/
 		1,	/*size*/
 		1,	/*craftable*/
 		0	/*raw*/
 	);
 
-INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
+INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemPCapacity, itemPPM, itemPConsumption, itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
 VALUES	(
 		'Titanium',
 		'Ti. Basic building material.',
 		'titanium',
 		'Basic Materials',
 		'Fabricator',
+		0,	/* Power Capacity */
+		0,	/* Power Per Minute */
+		0,	/* Power Consumption Per Minute */
 		0,	/*hull integrity*/
-		0,
+		0,	/*BioReactor Energy*/
 		1,	/*Build Time*/
 		1,	/*size*/
 		1,	/*craftable*/
@@ -168,75 +198,90 @@ VALUES	(
 
 /* Advanced Materials */
 
-INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
+INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemPCapacity, itemPPM, itemPConsumption, itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
 VALUES	(
 		'Aerogel',
 		'Light, Porous, dried gel. High heat insulation.',
 		'aerogel',
 		'Advanced Materials',
 		'Fabricator',
+		0,	/* Power Capacity */
+		0,	/* Power Per Minute */
+		0,	/* Power Consumption Per Minute */
 		0,	/*hull integrity*/
-		0,
+		0,	/*BioReactor Energy*/
 		1,	/*Build Time*/
 		1,	/*size*/
 		1,	/*craftable*/
 		0	/*raw*/
 	);
 
-INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
+INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemPCapacity, itemPPM, itemPConsumption, itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
 VALUES	(
 		'Benzene',
 		'C6H6. Oil and solvent applications.',
 		'benzene',
 		'Advanced Materials',
 		'Fabricator',
+		0,	/* Power Capacity */
+		0,	/* Power Per Minute */
+		0,	/* Power Consumption Per Minute */
 		0,	/*hull integrity*/
-		0,
+		0,	/*BioReactor Energy*/
 		1,	/*Build Time*/
 		1,	/*size*/
 		1,	/*craftable*/
 		0	/*raw*/
 	);
 
-INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
+INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemPCapacity, itemPPM, itemPConsumption, itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
 VALUES	(
 		'Hydrochloric Acid',
 		'HCI. Powerful acidic compound. Employ due care.',
 		'hydrochloricacid',
 		'Advanced Materials',
 		'Fabricator',
-		0, /*hull integrity*/
-		0,
+		0,	/* Power Capacity */
+		0,	/* Power Per Minute */
+		0,	/* Power Consumption Per Minute */
+		0,	/*hull integrity*/
+		0,	/*BioReactor Energy*/
 		1,	/*Build Time*/
 		1,	/*size*/
 		1,	/*craftable*/
 		0	/*raw*/
 	);
 	
-INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
+INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemPCapacity, itemPPM, itemPConsumption, itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
 VALUES	(
 		'Polyaniline',
 		'High conductance polymer.',
 		'polyaniline',
 		'Advanced Materials',
 		'Fabricator',
-		0, /*hull integrity*/
-		0,
+		0,	/* Power Capacity */
+		0,	/* Power Per Minute */
+		0,	/* Power Consumption Per Minute */
+		0,	/*hull integrity*/
+		0,	/*BioReactor Energy*/
 		4,	/*Build Time*/
 		1,	/*size*/
 		1,	/*craftable*/
 		0	/*raw*/
 	);
 	
-INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
+INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemPCapacity, itemPPM, itemPConsumption, itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
 VALUES	(
 		'Synthetic Fibers',
 		'High-strength synthetic fibers.',
 		'aramidfibers',
 		'Advanced Materials',
 		'Fabricator',
-		0, /*hull integrity*/
-		0,
+		0,	/* Power Capacity */
+		0,	/* Power Per Minute */
+		0,	/* Power Consumption Per Minute */
+		0,	/*hull integrity*/
+		0,	/*BioReactor Energy*/
 		4,	/*Build Time*/
 		1,	/*size*/
 		1,	/*craftable*/
@@ -245,15 +290,18 @@ VALUES	(
 
 /* Base Pieces */
 
-INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
+INSERT INTO item ( itemName, itemDesc, itemGameId, itemMenu, itemMadeWith,itemPCapacity, itemPPM, itemPConsumption, itemHullIntegrity, itemEnergy, itemBuildTime,itemSize, itemIsCraftable, itemIsRaw )
 VALUES	(
 		'Synthetic Fibers',
 		'High-strength synthetic fibers.',
 		'aramidfibers',
 		'Advanced Materials',
 		'Fabricator',
-		0, /*hull integrity*/
-		0,
+		0,	/* Power Capacity */
+		0,	/* Power Per Minute */
+		0,	/* Power Consumption Per Minute */
+		0,	/*hull integrity*/
+		0,	/*BioReactor Energy*/
 		4,	/*Build Time*/
 		1,	/*size*/
 		1,	/*craftable*/
